574ddd55077ca93e5f1d8d09b3d0c40f9bfd2d2b
Fleur41 <Muriithikings456@gmail.com>
2023-01-19 14:02:07 +0300

    Comleted lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return(
+  <div>
+    <ul>
+    <li> <NavLink to="/"  activeStyle={{color:"green"}}>
+        Home
+      </NavLink></li>
+      <li> <NavLink to="/movies"  activeStyle={{color:"green"}}>
+        Movies
+      </NavLink></li>
+      <li> <NavLink to="/directors"  activeStyle={{color:"green"}}>
+        Directors
+      </NavLink></li>
+      <li> <NavLink to="/actors"  activeStyle={{color:"green"}}>
+        Actors
+      </NavLink></li>
+    </ul>
+  </div>
+  )
 }
 
 export default NavBar;
